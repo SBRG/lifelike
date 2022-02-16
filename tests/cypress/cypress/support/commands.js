a7c20e308d5601539f03806f77ce7b6a7e77c1cd
David Regla <dreglad@gmail.com>
2022-02-15 23:19:50 -0600

    test: update browser test specs

--- tests/cypress/cypress/support/commands.js
+++ tests/cypress/cypress/support/commands.js
@@ -11,6 +11,32 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("loginByPassword", (email, password) => {
+  cy.log(`Logging in as ${email}`);
+  cy.request("POST", Cypress.env("auth_login_url"), { email, password }).then(
+    ({ body }) => {
+      cy.debug(`Login response: ${JSON.stringify(body)}`);
+      const { accessToken, refreshToken, user } = body;
+      const auth = { loggedIn: true, targetUrl: "/", user };
+      window.localStorage.setItem("auth", JSON.stringify(auth));
+      window.localStorage.setItem("authId", user.id);
+      window.localStorage.setItem("expires_at", accessToken.exp);
+      window.localStorage.setItem("access_jwt", accessToken.token);
+      window.localStorage.setItem("refresh_jwt", refreshToken.token);
+    }
+  );
+
+  // Navigate to workbench
+  return cy.visit("/workspaces/local");
+});
+
+Cypress.Commands.add("clickMenuItem", (item) => {
+  return cy.get(`[ngbtooltip="${item}`).click();
+});
+
+Cypress.Commands.add("getActiveTab", () => {
+  return cy.get('[ng-reflect-active="true"]').should("be.visible");
+});
 //
 //
 // -- This is a child command --
