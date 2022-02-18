385be66429e5210978e9a9ab926239b997cc0050
David Regla <dreglad@gmail.com>
2022-02-18 13:15:06 -0600

    graphdb migrator as container image [skip ci]

---
+++ graph-db/migrator/README.md
@@ -0,0 +1,40 @@
+# Lifelike Graph DB migrator
+
+Liquibase based migrator for Neo4j
+
+## Run with Docker
+
+To run migrations from Lifelike Docker registry latest image, you can run the following command:
+
+```bash
+# Update migrations
+docker run --rm \
+  --env NEO4J_HOST=neo4j:7687 \
+  --env NEO4J_PASSWORD=password \
+  --env AZURE_ACCOUNT_STORAGE_NAME=lifelike \
+  --env AZURE_ACCOUNT_STORAGE_KEY=<storage-key> \
+  --env CHANGELOG_DIR=lifelike-graph
+  ghcr.io/sbrg/lifelike-graphdb-migrator:latest
+```
+
+### Environment Variables
+
+| Variable                   | Default        | Comment                             |
+| -------------------------- | -------------- | ----------------------------------- |
+| CHANGELOG_DIR              | lifelike-graph | Master changelog directory path     |
+| NEO4J_HOST                 |                | Host and port of Neo4j instance     |
+| NEO4J_USERNAME             | neo4j          | Neo4j authentication username       |
+| NEO4J_PASSWORD             | password       | Neo4j authentication password       |
+| NEO4J_DATABASE             | neo4j          | Neo4j target database               |
+| STORAGE_TYPE               | azure          | Remote storage type to get TSV data |
+| AZURE_ACCOUNT_STORAGE_NAME |                | Azure Storage Account Name          |
+| AZURE_ACCOUNT_STORAGE_KEY  |                | Azure Storage Key                   |
+| LOG_LEVEL                  | WARNING        | Liquibase log level                 |
+
+### Docker build arguments
+
+| Argument                | Default value | Comment |
+| ----------------------- | ------------- | ------- |
+| LIQUIBASE_IMAGE_TAG     | 4.6 | Tag of the [Liquibase Docker image](https://hub.docker.com/r/liquibase/liquibase/tags) to use |
+| LIQUIBASE_NEO4J_VERSION | 4.6.2 | [Liquibase-Neo4j plugin](https://github.com/liquibase/liquibase-neo4j) version to install |
+| NEO4J_JDBC_VERSION      | 4.0.5 | Neo4j JDBC driver version |
---
+++ graph-db/migrator/docker-entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+
+## This is a modified version of the official Docker entrypoint script.
+## Ref: https://github.com/liquibase/docker/blob/ff6b4b8753d4a8a755521bf0e99bf3f03ecbfa08/docker-entrypoint.sh
+
+if type "$1" > /dev/null 2>&1; then
+  ## First argument is an actual OS command. Run it
+  exec "$@"
+else
+  if [[ "$*" == *--defaultsFile* ]] || [[ "$*" == *--defaults-file* ]] || [[ "$*" == *--version* ]]; then
+    ## Just run as-is
+    liquibase "$@"
+  else
+    ## Validate envrioment variables
+    if [ -z "$NEO4J_HOST" ]; then
+      echo "NEO4J_HOST environment variable is not set. Please set it to the hostname or IP address of the Neo4j server."
+      exit 1
+    elif [[ "$NEO4J_HOST" != *":"* ]]; then
+      ## If no port is specified, use the default one
+      NEO4J_HOST="$NEO4J_HOST:7687"
+    fi
+    if [ "$STORAGE_TYPE" != "azure" ]; then
+      echo "STORAGE_TYPE environment is set to an invalid valie. `azure` is currently only supported."
+      exit 1
+    fi
+    if [ -z "$AZURE_ACCOUNT_STORAGE_KEY" ]; then
+      echo "AZURE_ACCOUNT_STORAGE_KEY environment variable is not set. Please set it to the storage account key."
+      exit 1
+    fi
+
+    ## Wait until Neo4j is available
+    ./wait-for-it.sh "${NEO4J_HOST}" --timeout=600 -- echo "Neo4j is up"
+
+    ## Include standard defaultsFile
+    liquibase \
+      --defaults-file=/liquibase/liquibase.docker.properties \
+      --changelog-file=${CHANGELOG_FILE} \
+      --url=jdbc:neo4j:bolt://$NEO4J_HOST?database=${NEO4J_DATABASE:-neo4j} \
+      --username=$NEO4J_USERNAME \
+      --password=$NEO4J_PASSWORD \
+      --log-level=$LOG_LEVEL \
+      "$@" \
+      -Dliquibase.hub.mode=off \
+      -Dneo4jHost=bolt://$NEO4J_HOST \
+      -Dneo4jCredentials=$NEO4J_USERNAME,$NEO4J_PASSWORD \
+      -Dneo4jDatabase=${NEO4J_DATABASE:-neo4j} \
+      -DazureStorageName=$AZURE_ACCOUNT_STORAGE_NAME \
+      -DazureStorageKey=$AZURE_ACCOUNT_STORAGE_KEY \
+      -DlocalSaveFileDir=/tmp
+  fi
+fi
